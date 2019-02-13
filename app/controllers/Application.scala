5606b2077946bf2d40c3469455cb9466587eeb3d
Piotr Aleksander Styczyński <piotrsty1@gmail.com>
2019-02-13 11:06:07 +0100

    Remove dead endpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -48,23 +48,4 @@ object Application extends Controller {
     }
   }
 
-  def db = Action {
-    var out = ""
-    val conn = DB.getConnection()
-    try {
-      val stmt = conn.createStatement
-
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
-
-      val rs = stmt.executeQuery("SELECT tick FROM ticks")
-
-      while (rs.next) {
-        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
-      }
-    } finally {
-      conn.close()
-    }
-    Ok(out)
-  }
 }
